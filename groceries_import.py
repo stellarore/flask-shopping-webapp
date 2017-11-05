6fd7a0bca4bd1e8a3c216bef6137669c3cddd1da
paltryore <paltryore@user.noreply.github.com>
2017-11-05 12:16:36 -0500

    imported

--- groceries_import.py
+++ groceries_import.py
@@ -6,22 +6,34 @@ with open("groceries_import.csv") as f:
         if line[0] != "#":
             groceries.append(line.rstrip().split(','))
 
-for i in range(5):
-    print(groceries[i])
+conn = sqlite3.connect("grocerylist.db")
+cursor = conn.cursor()
 
+for groc in groceries:
+    # departments:
+    # produce = 1-Produce
+    # bakery = 2-Bakery
+    # meat = 3-Meat
+    # soda = 4-Soda
+    # snacks = 5-Snacks
+    # dry = 6-DryGoods
+    # misc = 7-Misc
+    # froz = 8-Frozen
+    # dairy = 9-Dairy
+    conv = {'produce':'1-Produce',
+            'bakery':'2-Bakery',
+            'meat':'3-Meat',
+            'soda':'4-Soda',
+            'snacks':'5-Snacks',
+            'dry':'6-DryGoods',
+            'misc':'7-Misc',
+            'froz':'8-Frozen',
+            'dairy':'9-Dairy'}
 
+    # print(groc[0],1,conv[groc[1]])
 
+    cursor.execute("INSERT INTO groceries (item, number, department) VALUES (?,?,?)", [groc[0],1,conv[groc[1]]])
 
-
-# conn = sqlite3.connect("grocerylist.db")
-#
-# cursor = conn.cursor()
-#
-# cursor.execute("ADD")
-#
-#
-#
-#
-#
-# conn.commit()
-# conn.close()
+conn.commit()
+conn.close()
+print('finish')
